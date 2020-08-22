0f6fcc642392c5a12b4fe283d79240371b832d23
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-08-22 13:30:53 +0300

    добавил менюшку сверху

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "materialize-css";
 import { useRoutes } from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
 
 function App() {
     const routes = useRoutes(true);
+    const isAuthenticated = 1;
     return (
         <Router>
+            {isAuthenticated === 1 && <NavBar />}
             <div className="container">{routes}</div>
         </Router>
     );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const NavBar = () => {
     return (
         <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo">
-                    Logo
-                </a>
+            <div
+                class="nav-wrapper blue darken-1"
+                style={{ padding: "0 2rem" }}
+            >
+                <span class="brand-logo">PostMen</span>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li>
-                        <a href="sass.html">Sass</a>
+                        <NavLink to="/postmen">Посты</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/album">Альбом</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/menu">Меню</NavLink>
+                    </li>
+                    <li>
+                        <a href="/">Выйти</a>
                     </li>
                 </ul>
             </div>
