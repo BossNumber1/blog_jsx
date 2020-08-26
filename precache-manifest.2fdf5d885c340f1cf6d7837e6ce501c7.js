247ee1a212692ee683bb05864917d93f43b2a5ff
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-08-26 11:19:28 +0300

    Updates

---
+++ precache-manifest.2fdf5d885c340f1cf6d7837e6ce501c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45f50024de84f4f5fe3fcd7ab006cde",
+    "url": "/blog_jsx/index.html"
+  },
+  {
+    "revision": "fda8f30e85a62a79bc76",
+    "url": "/blog_jsx/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "7ab514be037d5a8b7b3b",
+    "url": "/blog_jsx/static/css/main.61317492.chunk.css"
+  },
+  {
+    "revision": "fda8f30e85a62a79bc76",
+    "url": "/blog_jsx/static/js/2.c4dccc5d.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/blog_jsx/static/js/2.c4dccc5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab514be037d5a8b7b3b",
+    "url": "/blog_jsx/static/js/main.2ec97ac7.chunk.js"
+  },
+  {
+    "revision": "d806db8234b20a64d564",
+    "url": "/blog_jsx/static/js/runtime-main.c967067a.js"
+  }
+]);
