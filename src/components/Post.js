62d77b50401f9931d1ce9dd96d9c47d496960710
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-09-03 21:34:14 +0300

    улучшил профиль

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default ({ post }) => {
     return (
-        <div className="media">
+        <div className="media pb-4">
             <div className="media-body">
-                <h5 className="mt-0 mb-1">{post.title}</h5>
+                <h5 className="mb-1">{post.title}</h5>
                 <p className="card-text pt-3">{post.messagePost}</p>
                 <p className="card-text">
                     <small className="text-muted">{post.time}</small>
