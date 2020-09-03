127f7e307b861458f7db589b6d7854073e6d3734
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-09-03 22:07:24 +0300

    обрамил пост

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,23 @@
 import React from "react";
+import s from "./post.module.css";
 
 export default ({ post }) => {
     return (
-        <div className="media pb-4">
-            <div className="media-body">
-                <h5 className="mb-1">{post.title}</h5>
-                <p className="card-text pt-3">{post.messagePost}</p>
-                <p className="card-text">
-                    <small className="text-muted">{post.time}</small>
-                </p>
+        <div className={s.postBlock}>
+            <div className="media pb-4">
+                <div className="media-body">
+                    <h5 className="mb-1">{post.title}</h5>
+                    <p className="card-text pt-3">{post.messagePost}</p>
+                    <p className="card-text">
+                        <small className="text-muted">{post.time}</small>
+                    </p>
+                </div>
+                <img
+                    src={post.fileImg}
+                    alt="картинка к посту"
+                    style={{ width: 200, borderRadius: 5 }}
+                />
             </div>
-            <img
-                src={post.fileImg}
-                alt="картинка к посту"
-                style={{ width: 200, borderRadius: 5 }}
-            />
         </div>
     );
 };
