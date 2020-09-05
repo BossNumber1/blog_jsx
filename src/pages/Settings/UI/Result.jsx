ee230e8bba6c03dcff01476ecd6775308c5a0614
angelochecheCHEK <57631540+angelochecheCHEK@users.noreply.github.com>
2020-09-05 11:40:09 +0300

    сделал шаблонную аватарку

--- src/pages/Settings/UI/Result.jsx
+++ src/pages/Settings/UI/Result.jsx
@@ -13,10 +13,12 @@ export default ({ fileImg, firstName, secondName, age, country }) => {
                                 <img src={fileImg} alt="аватарка" />
                             </div>
                         ) : (
-                            <img
-                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTMyJn14ZEFjpLLMuEXcoz-YmbrtUyTvS7kFw&usqp=CAU"
-                                alt="аватарка"
-                            />
+                            <div className={s.avaNoume}>
+                                <img
+                                    src="https://www.meme-arsenal.com/memes/5b1fb1df3ef1f1b0369618d6a33017d4.jpg"
+                                    alt="без аватарки"
+                                />
+                            </div>
                         )}
                     </div>
                     <div className="card-content">
